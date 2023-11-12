619126d1e93c49ad2381abaaa458d10be9723640
AhmetAzizov <ahmet.azizov@gmail.com>
2023-11-13 00:46:07 +0100

    Added function for sorting notes based on their creation and update dates

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { Row, Container, Col, Button } from 'react-bootstrap';
 import Note from './Note';
 import { Note as NoteModel } from './models/note';
 import utilsStyle from "./styles/utils.module.css";
+import styles from "./styles/app.module.css";
 import AddNoteDialog from './AddNoteDialog';
+import { AiOutlineFileAdd } from "react-icons/ai";
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
@@ -16,6 +18,11 @@ function App() {
 
   async function getNotes() {
     const notes = await fetchNotes();
+    notes.sort((a, b) => {
+      const date1 = new Date(a.updatedAt > a.createdAt ? a.updatedAt : a.createdAt).getTime();
+      const date2 = new Date(b.updatedAt > b.createdAt ? b.updatedAt : b.createdAt).getTime();
+      return date2 - date1;
+    });
     setNotes(notes);
   }
 
@@ -27,17 +34,20 @@ function App() {
   return (
     <>
       <Button
-        className={`${utilsStyle.centerItem} my-4`}
+        className={`${utilsStyle.centerItem} ${styles.addButton} my-4`}
         onClick={() => setShowAddNoteDialog(true)}
       >
+        <AiOutlineFileAdd />
         Add new note
       </Button>
-      <Container>
+      <Container className='pb-4'>
         <Row className='g-4' xs={1} sm={2} md={3} xl={4}>
           {
             notes.map((note, index) => (
               <Col key={index} >
-                <Note note={note} />
+                <Note
+                  note={note}
+                  onDelete={() => { getNotes() }} />
               </Col>
             ))
           }
